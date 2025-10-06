2943e5dc92255b7bb3e7635f427743e4c675dfc5
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-05 21:26:09 -0300

    adicionando querys customizadas

--- src/main/java/com/example/collab/repository/CollaboratorRepository.java
+++ src/main/java/com/example/collab/repository/CollaboratorRepository.java
@@ -1,9 +1,35 @@
 package com.example.collab.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.collab.model.Collaborator;
 
 public interface CollaboratorRepository extends JpaRepository<Collaborator, Long> {
+
+    Optional<Collaborator> findByMatricula(String matricula);
+
+    Optional<Collaborator> findByDepartamento(String departamento);
+
+    Optional<Collaborator> findByCargo(String cargo);
+
+    Optional<Collaborator> findByTipoContrato(String tipoContrato);
+
+    Optional<Collaborator> findByCargaHoraria(String cargaHoraria);
+
+    Optional<Collaborator> findByCPF(String CPF);
+
+    Optional<Collaborator> findByRG(String RG);
+
+    Optional<Collaborator> findByBanco(String banco);
+
+    Optional<Collaborator> findByConta(String conta);
+
+    Optional<Collaborator> findByPix(String pix);
+
+    Optional<Collaborator> findByNome(String nome);
+
+    Optional<Collaborator> findByEmail(String email);
 
 }
