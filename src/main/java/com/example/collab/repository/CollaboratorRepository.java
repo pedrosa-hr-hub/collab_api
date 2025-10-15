42a0b5b31bfb58c532605aa2741157710b3e60c6
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-15 20:51:44 -0300

    implementando a função de excluir colaborador pela matricula.

--- src/main/java/com/example/collab/repository/CollaboratorRepository.java
+++ src/main/java/com/example/collab/repository/CollaboratorRepository.java
@@ -40,4 +40,6 @@ public interface CollaboratorRepository extends JpaRepository<Collaborator, Long
 
     Optional<Collaborator> findByEmail(String email);
 
+    void deleteByMatricula(Integer matricula);
+
 }
