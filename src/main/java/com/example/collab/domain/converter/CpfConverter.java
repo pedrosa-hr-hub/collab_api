a07169a30f5e43b804b3dfc4f5a257a193ddb5ce
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-22 21:26:41 -0300

    criando converter para CPF ser entendido no banco.

---
+++ src/main/java/com/example/collab/domain/converter/CpfConverter.java
@@ -0,0 +1,20 @@
+package com.example.collab.domain.converter;
+
+import com.example.collab.domain.CPF;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class CpfConverter implements AttributeConverter<CPF, String> {
+
+    @Override
+    public String convertToDatabaseColumn(CPF attribute) {
+        return attribute != null ? attribute.getCpf() : null;
+    }
+
+    @Override
+    public CPF convertToEntityAttribute(String dbData) {
+        return dbData != null ? new CPF(dbData) : null;
+    }
+}
