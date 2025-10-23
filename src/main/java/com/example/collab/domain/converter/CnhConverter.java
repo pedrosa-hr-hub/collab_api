0793dd9f8be0c094d35b5febd86d63e9e1dd6433
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-23 20:58:42 -0300

    criando converter para o JPA entender a classe.

---
+++ src/main/java/com/example/collab/domain/converter/CnhConverter.java
@@ -0,0 +1,20 @@
+package com.example.collab.domain.converter;
+
+import com.example.collab.domain.CNH;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class CnhConverter implements AttributeConverter<CNH, String> {
+
+    @Override
+    public String convertToDatabaseColumn(CNH attribute) {
+        return attribute != null ? attribute.getCnh() : null;
+    }
+
+    @Override
+    public CNH convertToEntityAttribute(String dbData) {
+        return dbData != null ? new CNH(dbData) : null;
+    }
+}
