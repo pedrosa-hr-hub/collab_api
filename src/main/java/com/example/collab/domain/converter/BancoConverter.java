45b2bc22acbfda5dc203d1c42242e86e00bf615e
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-23 22:11:17 -0300

    implementando conversor JPA para banco.

---
+++ src/main/java/com/example/collab/domain/converter/BancoConverter.java
@@ -0,0 +1,20 @@
+package com.example.collab.domain.converter;
+
+import com.example.collab.domain.Banco;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class BancoConverter implements AttributeConverter<Banco, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Banco attribute) {
+        return attribute != null ? attribute.getCodigo() : null;
+    }
+
+    @Override
+    public Banco convertToEntityAttribute(String dbData) {
+        return dbData != null ? new Banco(dbData) : null;
+    }
+}
