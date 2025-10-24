0c21196307bddfa791acf1a0fc06f41634deceed
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-23 22:11:59 -0300

    implementando conversor JPA para PIX

---
+++ src/main/java/com/example/collab/domain/converter/PIXConverter.java
@@ -0,0 +1,20 @@
+package com.example.collab.domain.converter;
+
+import com.example.collab.domain.PIX;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class PIXConverter implements AttributeConverter<PIX, String> {
+
+    @Override
+    public String convertToDatabaseColumn(PIX attribute) {
+        return attribute != null ? attribute.getChave() : null;
+    }
+
+    @Override
+    public PIX convertToEntityAttribute(String dbData) {
+        return dbData != null ? new PIX(dbData) : null;
+    }
+}
