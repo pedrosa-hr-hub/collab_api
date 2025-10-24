07e2bc6152bdf432f19cd8f83a443f2f9d2faf9b
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-23 22:11:53 -0300

    implementando conversor JPA para TipoConta

---
+++ src/main/java/com/example/collab/domain/converter/TipoContaConverter.java
@@ -0,0 +1,20 @@
+package com.example.collab.domain.converter;
+
+import com.example.collab.domain.TipoConta;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class TipoContaConverter implements AttributeConverter<TipoConta, String> {
+
+    @Override
+    public String convertToDatabaseColumn(TipoConta attribute) {
+        return attribute != null ? attribute.getTipo() : null;
+    }
+
+    @Override
+    public TipoConta convertToEntityAttribute(String dbData) {
+        return dbData != null ? new TipoConta(dbData) : null;
+    }
+}
