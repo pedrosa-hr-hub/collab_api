86e6f3baf36909225b224878dc187383bb06dade
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-23 21:21:32 -0300

    criando conversor para a classe TituloEleitor, implementando SRP

---
+++ src/main/java/com/example/collab/domain/converter/TituloEleitorConverter.java
@@ -0,0 +1,20 @@
+package com.example.collab.domain.converter;
+
+import com.example.collab.domain.TituloEleitor;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class TituloEleitorConverter implements AttributeConverter<TituloEleitor, String> {
+
+    @Override
+    public String convertToDatabaseColumn(TituloEleitor attribute) {
+        return attribute != null ? attribute.getTitulo() : null;
+    }
+
+    @Override
+    public TituloEleitor convertToEntityAttribute(String dbData) {
+        return dbData != null ? new TituloEleitor(dbData) : null;
+    }
+}
