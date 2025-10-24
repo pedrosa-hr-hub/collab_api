93839ad9112172a84524c7f2f3ed760272cb19a0
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-23 22:11:28 -0300

    implementando conversor JPA para CNPJ

---
+++ src/main/java/com/example/collab/domain/converter/CnpjConverter.java
@@ -0,0 +1,20 @@
+package com.example.collab.domain.converter;
+
+import com.example.collab.domain.CNPJ;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class CnpjConverter implements AttributeConverter<CNPJ, String> {
+
+    @Override
+    public String convertToDatabaseColumn(CNPJ attribute) {
+        return attribute != null ? attribute.getCnpj() : null;
+    }
+
+    @Override
+    public CNPJ convertToEntityAttribute(String dbData) {
+        return dbData != null ? new CNPJ(dbData) : null;
+    }
+}
