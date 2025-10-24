044275bbed50068fa6fbf150374b9455a3073bd8
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-23 21:19:45 -0300

    criando classe para representar Carteira de Trabalho, implementando SRP.

---
+++ src/main/java/com/example/collab/domain/CarteiraTrabalho.java
@@ -0,0 +1,30 @@
+package com.example.collab.domain;
+
+import lombok.Value;
+
+@Value
+public class CarteiraTrabalho {
+    String numero;
+
+    public CarteiraTrabalho(String numero) {
+        if (numero == null || numero.isBlank()) {
+            throw new IllegalArgumentException("Carteira de Trabalho deve ser informada");
+        }
+
+        // Remove caracteres não numéricos
+        String cleanedNumero = numero.replaceAll("\\D", "");
+
+        // Valida o tamanho da Carteira de Trabalho
+        if (cleanedNumero.length() != 11) {
+            throw new IllegalArgumentException(
+                    "Carteira de Trabalho deve conter 11 dígitos (7 para o número e 4 para a série)");
+        }
+
+        // Validação para evitar números com todos os dígitos iguais
+        if (cleanedNumero.matches("(\\d)\\1+")) {
+            throw new IllegalArgumentException("Carteira de Trabalho não pode conter todos os dígitos iguais");
+        }
+
+        this.numero = cleanedNumero;
+    }
+}
