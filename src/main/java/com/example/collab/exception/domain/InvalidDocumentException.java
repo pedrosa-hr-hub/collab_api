05a474fd0eb4f53be99b88b167547c4ffad0c90a
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-28 21:54:41 -0300

    criando exception InvalidDocumentException para tratamento de documentos inválidos (generico)

---
+++ src/main/java/com/example/collab/exception/domain/InvalidDocumentException.java
@@ -0,0 +1,9 @@
+package com.example.collab.exception.domain;
+
+import com.example.collab.exception.CollabApiException;
+
+public class InvalidDocumentException extends CollabApiException {
+    public InvalidDocumentException(String message) {
+        super(message);
+    }
+}
