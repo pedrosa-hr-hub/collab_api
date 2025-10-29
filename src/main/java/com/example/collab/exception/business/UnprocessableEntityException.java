be0df27bfee85345820ac65cf673a19dbc0988f1
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-28 21:53:26 -0300

    criando exception UnprocessableEntityException para tratar dados não processados.

---
+++ src/main/java/com/example/collab/exception/business/UnprocessableEntityException.java
@@ -0,0 +1,9 @@
+package com.example.collab.exception.business;
+
+import com.example.collab.exception.CollabApiException;
+
+public class UnprocessableEntityException extends CollabApiException {
+    public UnprocessableEntityException(String message) {
+        super(message);
+    }
+}
