effa5ca622647414db6c6f53e66fe336a3652137
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-29 22:30:57 -0300

    Ajuste no espaçamento, melhorar a visualização e a tornar leitura clara

--- src/main/java/com/example/collab/service/validation/CollaboratorValidator.java
+++ src/main/java/com/example/collab/service/validation/CollaboratorValidator.java
@@ -18,40 +18,47 @@ public class CollaboratorValidator {
 
     private CollaboratorRepository collaboratorRepository;
 
-    public void validateNewCollaboratorDocuments(String cpf, String RG, String CNH, String PIS, String carteiraTrabalho,
-            String tituloEleitor) {
+    public void validateNewCollaboratorDocuments(String cpf, String RG, String CNH, String PIS, String carteiraTrabalho, String tituloEleitor) {
 
         if (collaboratorRepository.findByCPF(cpf).isPresent()) {
+
             throw new DuplicatedCPFException("CPF already exists");
+
         }
 
         if (collaboratorRepository.findByRG(RG).isPresent()) {
+
             throw new DuplicatedRGException("RG already exists");
+
         }
 
         if (collaboratorRepository.findByCNH(CNH).isPresent()) {
+
             throw new DuplicatedCNHException("CNH already exists");
+
         }
 
         if (collaboratorRepository.findByPIS(PIS).isPresent()) {
+
             throw new DuplicatedPISException("PIS already exists");
+
         }
 
         if (collaboratorRepository.findByCarteiraTrabalho(carteiraTrabalho).isPresent()) {
+
             throw new DuplicatedWorkWalletException("Work Wallet already exists");
+
         }
 
         if (collaboratorRepository.findByTituloEleitor(tituloEleitor).isPresent()) {
+
             throw new DuplicatedVoteRegistrationException("Vote Registration Title already exists");
+            
         }
 
     }
 
     public void validateNewCollaboratorBank(String banco, String conta, String pix) {
-
-        if (collaboratorRepository.findByBanco(banco).isPresent()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Banco already exists");
-        }
 
         if (collaboratorRepository.findByConta(conta).isPresent()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Conta already exists");
