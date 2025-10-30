30ffd4dc76765d97965b3e57038044bb530af165
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-29 22:22:05 -0300

    removendo exceções InvalidCPFException, InvalidEmailException e InvalidTelefoneException pois não tem sentido na aplicação

--- src/main/java/com/example/collab/exception/domain/InvalidCPFException.java
+++
@@ -1,7 +0,0 @@
-package com.example.collab.exception.domain;
-
-public class InvalidCPFException extends InvalidDocumentException {
-    public InvalidCPFException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/collab/exception/domain/InvalidTelefoneException.java
+++
@@ -1,9 +0,0 @@
-package com.example.collab.exception.domain;
-
-import com.example.collab.exception.CollabApiException;
-
-public class InvalidTelefoneException extends CollabApiException {
-    public InvalidTelefoneException(String message) {
-        super(message);
-    }
-}
