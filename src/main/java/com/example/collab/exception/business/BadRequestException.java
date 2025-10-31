3fae2a5a963c2fa9f2b9e897c3a255d09a12b71a
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-30 22:25:54 -0300

    ajuste no espaçamento para tornar a leitura do codigo melhor.

--- src/main/java/com/example/collab/exception/business/BadRequestException.java
+++ src/main/java/com/example/collab/exception/business/BadRequestException.java
@@ -3,7 +3,10 @@ package com.example.collab.exception.business;
 import com.example.collab.exception.CollabApiException;
 
 public class BadRequestException extends CollabApiException {
+
     public BadRequestException(String message) {
+
         super(message);
+
     }
 }
