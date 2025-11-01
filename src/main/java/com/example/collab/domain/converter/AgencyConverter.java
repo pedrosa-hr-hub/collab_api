77f2c4ed7946ef5036725c3a903d12f5a2035d06
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-31 21:59:19 -0300

    Criar classes Agency e AgencyConverter com validações para o número da agência. (traduzindo codigo)

---
+++ src/main/java/com/example/collab/domain/converter/AgencyConverter.java
@@ -0,0 +1,22 @@
+package com.example.collab.domain.converter;
+
+import com.example.collab.domain.valueobject.bancario.Agency;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class AgencyConverter implements AttributeConverter<Agency, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Agency attribute) {
+
+        return attribute != null ? attribute.getNumber() : null;
+
+    }
+
+    @Override
+    public Agency convertToEntityAttribute(String dbData) {
+        return dbData != null ? new Agency(dbData) : null;
+    }
+}
