c44d76dfdb6003141bd4aa66a86200d0fbec02b8
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-10-31 22:16:43 -0300

    Ajustando espaçamento para ajudar na leitura do codigo.

--- src/main/java/com/example/collab/exception/domain/InvalidDocumentException.java
+++ src/main/java/com/example/collab/exception/domain/InvalidDocumentException.java
@@ -3,7 +3,10 @@ package com.example.collab.exception.domain;
 import com.example.collab.exception.CollabApiException;
 
 public class InvalidDocumentException extends CollabApiException {
+
     public InvalidDocumentException(String message) {
+
         super(message);
+
     }
 }
