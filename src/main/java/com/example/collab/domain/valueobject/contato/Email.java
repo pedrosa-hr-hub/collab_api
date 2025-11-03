5ece861a01a94307abbdc5890775ace961c52e1e
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-11-02 22:04:33 -0300

    Ajustando exptions e traduzindo codigo.

--- src/main/java/com/example/collab/domain/valueobject/contato/Email.java
+++ src/main/java/com/example/collab/domain/valueobject/contato/Email.java
@@ -5,22 +5,31 @@ import lombok.Value;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 
+import com.example.collab.exception.domain.InvalidDocumentException;
+
 @Value
 public class Email {
     String email;
 
     public Email(String email) {
+
         if (email == null || email.isBlank()) {
-            throw new IllegalArgumentException("E-mail deve ser informado");
+
+            throw new InvalidDocumentException("Email address must be provided.");
+
         }
         if (!isValid(email)) {
-            throw new IllegalArgumentException("Formato inválido: " + email);
+
+            throw new InvalidDocumentException("Invalid format: " + email);
+
         }
 
         this.email = email;
+
     }
 
     private boolean isValid(String email) {
+
         try {
 
             InternetAddress address = new InternetAddress(email);
@@ -34,5 +43,7 @@ public class Email {
             return false;
 
         }
+
     }
+
 }
