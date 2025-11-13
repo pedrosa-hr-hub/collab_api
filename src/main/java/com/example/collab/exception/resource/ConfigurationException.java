0081ed9b65ccbdfb21cbc24ed9e39c4c4aa3c984
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-11-12 22:30:31 -0300

    ajuste de PR, criando exception para configuração.

---
+++ src/main/java/com/example/collab/exception/resource/ConfigurationException.java
@@ -0,0 +1,13 @@
+package com.example.collab.exception.resource;
+
+import com.example.collab.exception.CollabApiException;
+
+public class ConfigurationException extends CollabApiException {
+
+    public ConfigurationException(String message) {
+
+        super(message);
+
+    }
+
+}
