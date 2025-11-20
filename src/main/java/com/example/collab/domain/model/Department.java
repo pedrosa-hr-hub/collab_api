163a80153d151ee545f921e7d34cbff434c30baf
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-11-20 20:42:18 -0300

    criando entidade para representação de departamento no banco de dados

---
+++ src/main/java/com/example/collab/domain/model/Department.java
@@ -0,0 +1,11 @@
+package com.example.collab.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department{
+
+}
