b253e1b308bc1ed898c566c8c0e63f31632a9715
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-11-20 20:46:41 -0300

    adição de um campo necessario para identificar o numero sem expor o ID

--- src/main/java/com/example/collab/domain/model/Department.java
+++ src/main/java/com/example/collab/domain/model/Department.java
@@ -1,11 +1,47 @@
 package com.example.collab.domain.model;
 
+import java.sql.Date;
+import java.util.List;
+
+import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-public class Department{
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private Integer number;
+
+    @Getter
+    @Setter
+    private Date initial_date;
+
+    @Getter
+    @Setter
+    private Date end_date;
+
+    @Getter
+    @Setter
+    private Integer chef_registration;
+
+    @Getter
+    @Setter
+    private List<Integer> chef_suport_registration;
 
 }
