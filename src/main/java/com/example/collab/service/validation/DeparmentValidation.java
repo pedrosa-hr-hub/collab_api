aee8590e6bd5355b864a6448ecada038dba1019c
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-11-21 21:05:21 -0300

    estrutura base para funções que vão ser utilizadas nas validações de negocio

--- src/main/java/com/example/collab/service/validation/DeparmentValidation.java
+++ src/main/java/com/example/collab/service/validation/DeparmentValidation.java
@@ -1,5 +1,7 @@
 package com.example.collab.service.validation; 
 
+import java.sql.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,5 +17,25 @@ public class DeparmentValidation {
 
         this.departmentRepository = departmentRepository;
 
-    }    
+    }
+
+    public void validateDepartmentCheff(String registration){
+
+    }
+
+    public void validateDepartmentSupportCheff(String registration){
+
+    }
+
+    public void validateDepartmentName(String name){
+
+    }
+
+    public void validateDepartmentNumber(Integer number){
+
+    }
+
+    public void validateDeparmenDate(Date intial_date, Date end_date){
+
+    }
 }
