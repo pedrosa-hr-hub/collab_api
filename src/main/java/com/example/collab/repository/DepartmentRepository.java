18e854b3a5e854892a99213ec244aeeea6294bb1
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-11-22 17:31:09 -0300

    adicionando consulta costumozada para numero do departamento

--- src/main/java/com/example/collab/repository/DepartmentRepository.java
+++ src/main/java/com/example/collab/repository/DepartmentRepository.java
@@ -1,7 +1,13 @@
 package com.example.collab.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.collab.domain.model.Department;
 
-public interface DepartmentRepository extends JpaRepository<Department, Long>{}
+public interface DepartmentRepository extends JpaRepository<Department, Long>{
+
+    Optional<Department> findByNumber(Integer mumber);
+
+}
