9c85b292e80bee25d24cd8ca56096df8f7227084
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-11-22 22:38:17 -0300

    apagando import não utlizado

--- src/main/java/com/example/collab/service/validation/DeparmentValidation.java
+++ src/main/java/com/example/collab/service/validation/DeparmentValidation.java
@@ -1,7 +1,5 @@
 package com.example.collab.service.validation; 
 
-import java.sql.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
