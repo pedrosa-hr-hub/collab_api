ed3b2cbcc4d567f6bcb93019c1eb3346ec6a72ce
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-11-25 21:42:30 -0300

    adicionando mapeamento entidade -> dto para que um campo dto corresponda a uma campo na entidade

--- src/main/java/com/example/collab/mapper/DepartmentMapper.java
+++ src/main/java/com/example/collab/mapper/DepartmentMapper.java
@@ -9,10 +9,28 @@ import com.example.collab.dto.response.DepartmentResponseDTO;
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface DepartmentMapper {
 
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "number", source = "number")
+    @Mapping(target = "initialDate", source = "initialDate")
+    @Mapping(target = "endDate", source = "endDate")
+    @Mapping(target = "managerRegistration", source = "managerRegistration")
+    @Mapping(target = "managerSupportRegistration", source = "managerSupportRegistration")
     Department toEntity(DepartmentRequestDTO dto);
 
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "number", source = "number")
+    @Mapping(target = "initialDate", source = "initialDate")
+    @Mapping(target = "endDate", source = "endDate")
+    @Mapping(target = "managerRegistration", source = "managerRegistration")
+    @Mapping(target = "managerSupportRegistration", source = "managerSupportRegistration")
     void updateEntity(@MappingTarget Department department, DepartmentRequestDTO dto);
 
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "number", source = "number")
+    @Mapping(target = "initialDate", source = "initialDate")
+    @Mapping(target = "endDate", source = "endDate")
+    @Mapping(target = "managerRegistration", source = "managerRegistration")
+    @Mapping(target = "managerSupportRegistration", source = "managerSupportRegistration")
     DepartmentResponseDTO toResponse(Department department);
 
 }
